24db99d1bc810d0c4e762febb52d1bd481bf7d46
LucaGerace <32212065+LucaGerace@users.noreply.github.com>
2017-09-23 16:46:06 -0400

    Update main.go

--- main.go
+++ main.go
@@ -7,57 +7,60 @@ import "fmt"
 type amp struct {
 	left  []float64
 	right []float64
-	beats []int
-	avE   float64
-	inE   []float64
-	tempE float64
 }
+var inE[] float64
+var tempE float64
+var avE float64
+var beatnum int = 0
+var beat[] int
 
-func NewAmp() *amp {
-	return &amp{
-	//TODO fill in the variables
-	}
-}
 
-func (a *amp) findbeats() {
-	for i := 0; i < len(amp{left}); i++ {
-		amp{tempE} += ((amp{left[i]} * amp{right[i]}) + (amp{right[i]} * amp{right[i]}))
-		if i%1024 == 0 {
-			amp{inE[i/1024]} = (amp{left[i]} * amp{left[i]}) + (amp{right[i]} * amp{right[i]})
-		}
+//func NewAmp(x int) *amp {
+	//TODO reads wav/mp3 and fills in amp left and right
 
-	}
-	amp{avE} = (1024 / 44032) * tempE
-	for i := 0; i < len(amp{left[i]}); i += 1024 {
-		if (amp{inE[i]} > (amp{avE} * 1.3)) {
-			amp{beat[i]} = 1
-			continue
-		}
-		amp{beat[i]} = 0
-	}
-}
-
-func main() {
+	
 	//shift buffer to the right 1024 samples, eliminate 'left 1024 samples, repeat until end of file
 	//XXX can't set entire array to a single value
 	//ampL[:1024] = 0
 	//ampR[:1024] = 0
 
-	for i := 0; i < (len(ampL) - 1024); i++ {
-		ampL[i] = ampL[i+1024]
-		ampR[i] = ampR[i+1024]
-	}
+//	for i := 0; i < (len(amp{left}) - 1024); i++ {
+//		amp{left[i]} = amp{left[i+1024]}
+//		amp{right[i]} = amp{right[i+1024]}
+//	}
 
-	for i := (len(ampL) - 1024); i < len(ampL); i++ {
-		ampL[i] = 0 //read amplitude of song
+//	for i := (len(ampL) - 1024); i < len(ampL); i++ {
+		//ampL[i] = //read amplitude of song
 		//ampR[i] = //read amplitude of song
+//	}
+//}
+
+
+func findbeats() {
+	for i := 0; i < len(amp{left}); i++ {
+		tempE += ((amp{left[i]} * amp{right[i]}) + (amp{right[i]} * amp{right[i]}))
+		if i%1024 == 0 {
+			inE[i/1024] = (amp{left[i]} * amp{left[i]}) + (amp{right[i]} * amp{right[i]})
+		}
+
 	}
+	avE = (1024 / 44032) * tempE
+	for i := 0; i < len(amp{left[i]}); i += 1024 {
+		if (amp{inE[i]} > (avE * 1.3)) {
+			beat[i] = 1
+			continue
+		}
+		beat[i] = 0
+	}
+}
+
+func main() {
+
 
 	//TODO iterate that function for the rest of the song
 
 	//divide number of beats by song length to determine BPM and print to screen
 
-	var beatnum int = 0
 	//var slen = //songlength in seconds
 
 	for i := 0; i < len(beat); i++ {
